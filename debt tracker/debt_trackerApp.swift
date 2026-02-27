52e74550e096e499077d9ca9201de3017d8bfb03
kevingamez <ks.gamez@uniandes.edu.co>
2026-02-27 00:19:46 -0500

    Initial Commit

---
+++ debt tracker/debt_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  debt_trackerApp.swift
+//  debt tracker
+//
+//  Created by Kevin Gamez on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct debt_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
