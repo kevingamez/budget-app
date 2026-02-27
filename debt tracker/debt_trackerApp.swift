c1b9289ec922bbe64cee1ed240a5042c8252cd7e
kevingamez <ks.gamez@uniandes.edu.co>
2026-02-27 03:22:03 -0500

    Auto-seed sample data in debug builds

--- debt tracker/debt_trackerApp.swift
+++ debt tracker/debt_trackerApp.swift
@@ -22,4 +22,27 @@ struct debt_trackerApp: App {
             DebtCategory.self,
         ])
     }
+
+    init() {
+        #if DEBUG
+        seedSampleDataIfNeeded()
+        #endif
+    }
+
+    #if DEBUG
+    private func seedSampleDataIfNeeded() {
+        let key = "debugSampleDataSeeded"
+        guard !UserDefaults.standard.bool(forKey: key) else { return }
+        Task { @MainActor in
+            do {
+                let container = try ModelContainer(for: Debt.self, Payment.self, Person.self, DebtCategory.self)
+                SampleDataService.seedSampleData(context: container.mainContext)
+                try container.mainContext.save()
+                UserDefaults.standard.set(true, forKey: key)
+            } catch {
+                print("Debug seed failed: \(error)")
+            }
+        }
+    }
+    #endif
 }
